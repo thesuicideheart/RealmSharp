4d6264e89eeac4f4ec0c75ab869cbb20c3f6586b
thesuicideheart <tillolgamer@gmail.com>
2019-10-24 03:29:43 +0200

    Started workign on scraping

---
+++ RealmSharp/DataStructures/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealmSharp.DataStructures
+{
+    public struct Player
+    {
+        public int Stars { get; set; }
+        public int Fame { get; set; }
+        public int Skins { get; set; }
+        public int Exp { get; set; }
+        public int Accountfame { get; set; }
+        public string Guild { get; set; }
+        public string GuildRank { get; set; }
+        public string DescriptionLine1 { get; set; }
+        public string DescriptionLine2 { get; set; }
+        public string DescriptionLine3 { get; set; }
+    }
+}
---
+++ RealmSharp/Scrapers/PlayerScraper.cs
@@ -0,0 +1,32 @@
+﻿using RealmSharp.DataStructures;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealmSharp.Scrapers
+{
+    public class PlayerScraper
+    {
+        public static Player ScrapePlayer(string url)
+        {
+            var player = new Player();
+
+            var browser = new ScrapingBrowser();
+            browser.AllowAutoRedirect = true;
+            browser.AllowMetaRedirect = true;
+
+            var page = browser.NavigateToPage(new Uri(url));
+
+            if(page != null)
+            {
+                var wantedElem = page.Html.CssSelect(".col-md-12");
+            }
+
+            return player;
+        }
+    }
+}
